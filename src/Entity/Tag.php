9a4ba1e8d33fc1d787886a74f78592e1e59a48f2
rejmond <sergei.shlyanin@gmail.com>
2019-03-14 16:32:28 +0700

    Подключен EasyAdmin.

--- src/Entity/Tag.php
+++ src/Entity/Tag.php
@@ -38,6 +38,11 @@ class Tag
         $this->children = new ArrayCollection();
     }
 
+    public function __toString()
+    {
+        return $this->getName();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
